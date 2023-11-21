bff252568a0834f3481f7cbab83674cdb14c5204
Udenkov Vitaly <v.udenkov@napoleonit.com>
2023-11-21 12:33:18 +0600

    Добавлены константы

---
+++ ShootEmUp (Dirty)/Assets/Scripts/Common/Consts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Common
+{
+    public class Consts
+    {
+        /// <summary>
+        /// Скорость движения объектов использующих MoveComponent
+        /// </summary>
+        public const float MoveComponentSpeed = 5f;
+
+        /// <summary>
+        /// Количество пуль в пуле
+        /// </summary>
+        public const int BulletPoolInitialCount = 50;
+
+        /// <summary>
+        /// Количество врагов в пуле
+        /// </summary>
+        public const int EnemyPoolInitialCount = 7;
+
+        /// <summary>
+        /// Минимальная дистация до позиции аттаки
+        /// </summary>
+        public const float ReachDistanseThreshold = 0.25f;
+    }
+}
