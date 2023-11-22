e036806d6b701595b9707ae7a116a8c6ba29d31e
Udenkov Vitaly <v.udenkov@napoleonit.com>
2023-11-22 12:16:08 +0600

    переделано на сериализованное свойство

--- ShootEmUp (Dirty)/Assets/Scripts/Components/TeamComponent.cs
+++ ShootEmUp (Dirty)/Assets/Scripts/Components/TeamComponent.cs
@@ -4,12 +4,7 @@ namespace Assets.Scripts.Components
 {
     public sealed class TeamComponent : MonoBehaviour
     {
-        public bool IsPlayer
-        {
-            get { return _isPlayer; }
-        }
-
-        [SerializeField]
-        private bool _isPlayer;
+        [field:SerializeField]
+        public bool IsPlayer { get; set; }
     }
 }
