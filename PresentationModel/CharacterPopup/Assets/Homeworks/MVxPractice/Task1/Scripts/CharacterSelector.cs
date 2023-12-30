735901b4706dd37a8072b01b341c869452631311
Udenkov Vitaly <v.udenkov@napoleonit.com>
2023-12-30 12:20:44 +0600

    Добавлены кнопки back и select

--- PresentationModel/CharacterPopup/Assets/Homeworks/MVxPractice/Task1/Scripts/CharacterSelector.cs
+++ PresentationModel/CharacterPopup/Assets/Homeworks/MVxPractice/Task1/Scripts/CharacterSelector.cs
@@ -1,11 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelector : MonoBehaviour
 {
     [SerializeField] private ScrollerView _scrollerView;
+    [SerializeField] private Button _backButton;
+    [SerializeField] private Button _selectButton;
 
     public void Show(ScrollerPresenter presenter)
     {
         _scrollerView.Initialize(presenter);
+
+        _backButton.onClick.AddListener(OnBackButtonClick);
+        _selectButton.onClick.AddListener(OnSelectButtonClick);
+    }
+
+    private void OnBackButtonClick()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnSelectButtonClick()
+    {
+        Debug.Log("Selected character: " + _scrollerView.MiddleIcon.Text.text);
+    }
+
+    private void OnDestroy()
+    {
+        _backButton.onClick.RemoveAllListeners();
+        _selectButton.onClick.RemoveAllListeners();
     }
 }
