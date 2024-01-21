1f0bb48c14cd954f31ded401192c32687953e9e1
Udenkov Vitaly <v.udenkov@napoleonit.com>
2024-01-21 14:51:12 +0600

    changed directory structure

--- AppStarter/Assets/Scripts/Abstractions/StepApplicationRunner.cs
+++
@@ -1,8 +0,0 @@
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-
-public abstract class StepApplicationRunner : MonoBehaviour
-{
-    public abstract string Title { get; }
-    public abstract UniTask WaitOnCompleted();
-}
--- AppStarter/Assets/Scripts/Abstractions/StepApplicationRunnerScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-
-public abstract class StepApplicationRunnerScriptableObject : ScriptableObject
-{
-    public abstract string Title { get; }
-    public abstract UniTask WaitOnCompleted();
-}
