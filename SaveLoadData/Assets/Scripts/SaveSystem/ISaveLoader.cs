20a362cf6b6c0d18532906bb77407ec8478c3340
Udenkov Vitaly <v.udenkov@napoleonit.com>
2024-01-21 16:03:31 +0600

    changed directory structure

--- SaveLoadData/Assets/Scripts/SaveSystem/ISaveLoader.cs
+++
@@ -1,7 +0,0 @@
-using Zenject;
-
-public interface ISaveLoader
-{
-    public void LoadGame(IGameRepository gameRepository, SceneContext sceneContext);
-    public void SaveGame(IGameRepository gameRepository, SceneContext sceneContext);
-}
