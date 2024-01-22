8012be5a75e93994bd54dcb1c6d769b45f5337e6
Udenkov Vitaly <v.udenkov@napoleonit.com>
2024-01-22 20:20:07 +0600

    removed unnecessary folder, added plugins and hometask files

--- PresentationModel/CharacterPopup/Assets/Homeworks/MVxPractice/Task2/Scripts/Data/ClickerCharacterData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ClickerCharacterData", menuName = "Data/ClickerCharacterData")]
-public class ClickerCharacterData : ScriptableObject
-{
-    [SerializeField] private string _name;
-    public string Name => _name;
-    public int Health = 100;
-}
