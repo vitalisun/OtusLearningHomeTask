cac53015d7b506bf591e917abf100f3233229250
Udenkov Vitaly <v.udenkov@napoleonit.com>
2024-02-14 10:24:28 +0600

    добавлен спецэффект для зомби - кровь при получении урона

---
+++ UnityProject/Assets/Game/Scripts/Zombi/ZombiVfx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Zombi
+{
+    public class ZombiVfx : MonoBehaviour
+    {
+        [SerializeField] private Zombi _zombi;
+
+        private ParticleSystem _particleSystem;
+
+        private void Awake()
+        {
+            _particleSystem = _zombi.GetComponentInChildren<ParticleSystem>();
+        }
+
+        private void OnEnable()
+        {
+            _zombi.TakeDamageEvent.Subscribe(PlayTakeDamageVfx);
+        }
+
+        private void OnDisable()
+        {
+            _zombi.TakeDamageEvent.Unsubscribe(PlayTakeDamageVfx);
+        }
+
+        private void PlayTakeDamageVfx(int damage)
+        {
+            _particleSystem.Play();
+        }
+    }
+}
